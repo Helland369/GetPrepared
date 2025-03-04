253a7f1cbb5e06490aa60f785b875a0d02d300db
Thomas Helland <thomas_helland@pm.me>
2025-03-04 12:53:17 +0100

    Added graceful shutdown to the simple go server

--- golang/simple_web_server/main.go
+++ golang/simple_web_server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -12,22 +16,26 @@ type person struct {
 	Age  int
 }
 
+// localhos:8080/
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the simple go web server!")
+	fmt.Fprintf(w, "Welcome to the simple go web server!\n")
 }
 
+// localhos:8080/about
 func handler_about(w http.ResponseWriter, r *http.Request) {
-	about := []string{"About this server:", "This is a very simple go web server", "This is very fun!"}
+	about := []string{"About this server:", "This is a very simple go web server", "This is very fun!\n"}
 	for _, item := range about {
 		fmt.Fprintln(w, item)
 	}
 }
 
+// localhos:8080/contact
 func handler_contact(w http.ResponseWriter, r *http.Request) {
-	contact := "Prøv oplysningen!"
+	contact := "This is the contact page. Currently there is no contact info, but you can try the yellow pages.\n"
 	fmt.Fprintf(w, contact)
 }
 
+// localhos:8080/json
 func handler_json(w http.ResponseWriter, r *http.Request) {
 	newPerson := person{
 		Name: "Ola",
@@ -37,11 +45,11 @@ func handler_json(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	} else {
-		//fmt.Fprintln(w, bs)
 		fmt.Fprintln(w, string(bs))
 	}
 }
 
+// localhos:8080/static
 func static_web_page() {
 	fileServer := http.FileServer(http.Dir("./static"))
 
@@ -50,22 +58,67 @@ func static_web_page() {
 	fmt.Println("Static file server is running at http:://localhost:8080/static/page1.html")
 }
 
-func go_routine_time() {
+// prints the current time every 5 sec in the terminal
+func go_routine_time(stopChan chan struct{}) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
 	for {
-		fmt.Println("Time: ", time.Now().Format("15:04:05"))
-		time.Sleep(5 * time.Second)
+		select {
+		case <-ticker.C:
+			fmt.Println("Time: ", time.Now().Format("15:04:05"))
+		case <-stopChan:
+			fmt.Println("Stopping time go routine...")
+			return
+		}
 	}
 }
 
 func main() {
 	static_web_page()
 
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/about", handler_about)
-	http.HandleFunc("/contact", handler_contact)
-	http.HandleFunc("/json", handler_json)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	mux.HandleFunc("/about", handler_about)
+	mux.HandleFunc("/contact", handler_contact)
+	mux.HandleFunc("/json", handler_json)
 
-	go go_routine_time()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
 
-	http.ListenAndServe(":8080", nil)
+	// chanel for handeling shutdown signal
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	// chanel for stoping go_routine_time
+	goroutineStop := make(chan struct{})
+
+	go go_routine_time(goroutineStop)
+
+	// start server in a go routine
+	go func() {
+		fmt.Println("Starting server...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("HTTP server listen and serve: %v\n", err)
+		}
+	}()
+
+	// wait for interupt signal
+	<-stopChan
+	fmt.Println("\nShutting down server...")
+
+	// stop the go routine
+	close(goroutineStop)
+
+	// Create context with timeout for server shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// graceful server shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server is shuting down... %v\n", err)
+	}
+	fmt.Println("Server stoped!")
 }
